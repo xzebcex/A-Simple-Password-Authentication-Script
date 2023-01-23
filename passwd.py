2b3ed2738332417fbf6e25442bad508a0f49db59
Aman Pandit <amanpandit3027@gmail.com>
2023-01-23 07:49:49 +0530

    Create passwd.py

---
+++ passwd.py
@@ -0,0 +1,9 @@
+# A Simple Password Authentication Script
+
+print('Enter password')
+typed_password = input()
+if typed_password == 'password':
+    print('Access Granted')
+else:
+    print('Access Denied')
+print('Done')
