716caf37f01539f6f9435716f1e21509af347594
Aman Pandit <amanpandit3027@gmail.com>
2023-02-25 09:55:05 +0530

    Update passwd.py
    added a colon in input() function.

--- passwd.py
+++ passwd.py
@@ -1,6 +1,6 @@
 # A Simple Password Authentication Script
 
-typed_password = input('Enter password')
+typed_password = input('Enter password: ')
 if typed_password == 'password':
     print('Access Granted')
 else:
